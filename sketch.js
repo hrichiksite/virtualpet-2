8952dcbbcbf6b1c56fa17db11820836f689410f7
hrichiksite <hrichik.site@gmail.com>
2021-11-25 17:02:21 +0530

    init

---
+++ sketch.js
@@ -0,0 +1,87 @@
+var dog,sadDog,happyDog,garden,washroom, database;
+var foodS,foodStock;
+var fedTime,lastFed,currentTime;
+var feed,addFood;
+var foodObj;
+var gameState,readState;
+
+function preload(){
+sadDog=loadImage("images/Dog.png");
+happyDog=loadImage("images/happy dog.png");
+garden=loadImage("images/Garden.png");
+washroom=loadImage("images/Wash Room.png");
+bedroom=loadImage("images/Bed Room.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(400,500);
+  
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+
+ /* //read game state from database
+  readState=database.ref('gameState');
+  readState.on("value",function(data){
+    gameState=data.val();
+  });*/
+   
+  dog=createSprite(200,400,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+  
+  feed=createButton("Feed the dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+}
+
+function draw() {
+  foodObj.display()
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
+  
+
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+//function to update food stock and last fed time
+function feedDog(){
+  dog.addImage(happyDog);
+
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour(),
+    gameState:"Hungry"
+  })
+}
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
+
+//update gameState
+function update(state){
+  database.ref('/').update({
+    gameState:state
+  })
+}
